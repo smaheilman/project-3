3f72ec3414fe3e13a4327ef71c0b7b8628f4f517
Jake <xxjakexx14@gmail.com>
2022-01-26 19:46:32 -0500

    added user-routes/added port to connection.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
-const path = require('path');
+// const path = require('path');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-// serve up react front-end in production
+// // serve up react front-end in production
+// router.use((req, res) => {
+//   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+// });
+
 router.use((req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/index.html'));
+  res.status(404).end();
 });
 
 module.exports = router;
