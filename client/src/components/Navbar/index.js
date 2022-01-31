a4c193c469f97e1d02ec17a686d953eb2fe81680
Jake <xxjakexx14@gmail.com>
2022-01-31 18:20:24 -0500

    removed unused code in navbar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -26,17 +26,6 @@ const Navbar = () => {
             <a className="image-link" href="/">
               <img alt="jake" src={require("../../assets/pfp_placeholder.png")}></img>
             </a>
-            <div className="header-links">
-              <a href="https://github.com/Jakeology" target="_blank" rel="noreferrer">
-                <i className="fa-brands fa-github"></i>
-              </a>
-              <a href="https://www.linkedin.com/in/jacob-bartoletta-9b1566216/" target="_blank" rel="noreferrer">
-                <i className="fa-brands fa-linkedin"></i>
-              </a>
-              <a href="https://stackoverflow.com/users/16644863/jacob-b" target="_blank" rel="noreferrer">
-                <i className="fa-brands fa-stack-overflow"></i>
-              </a>
-            </div>
           </div>
           <div className="header-menu">
             <Link to="/">Home</Link>
