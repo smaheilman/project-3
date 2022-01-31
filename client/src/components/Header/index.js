254a21f827e6dafba8a1a1af22199991fdc9bc24
Shmee <smaheilman@gmail.com>
2022-01-31 18:29:43 -0500

    merge

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -6,12 +6,16 @@ import { Typography } from '@mui/material';
 
 const Header = () => {
   const location = useLocation();
+  console.log(location);
+
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
 
         <Link to={Home}>
-          <h1>Imperium</h1>
+          <Typography>
+          Imperium
+          </Typography>
         </Link>
       </div>
       <ul>
