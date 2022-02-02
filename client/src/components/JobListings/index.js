fc4cc80190b4c057226e8dcce92cca7630f193ed
Jake <xxjakexx14@gmail.com>
2022-02-02 13:02:41 -0500

    formatted JobListing comp and removed unused vars

--- client/src/components/JobListings/index.js
+++ client/src/components/JobListings/index.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { getUsers, getLoggedUser, getJobs, addBid } from '../../utils/API';
-import Auth from '../../utils/auth'
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { getJobs, addBid } from '../../utils/API';
+import Auth from "../../utils/auth";
+import { Jumbotron, Container, CardColumns, Card, Button } from "react-bootstrap";
 
 const JobList = (props) => {
-
   const [jobData, setJobData] = useState([]);
 
   // use this to determine if `useEffect()` hook needs to run again
@@ -14,11 +13,10 @@ const JobList = (props) => {
   useEffect(() => {
     const getJobData = async () => {
       try {
-
         const response = await getJobs(jobData);
 
         if (!response.ok) {
-          throw new Error('something went wrong!');
+          throw new Error("something went wrong!");
         }
 
         const job = await response.json();
@@ -35,25 +33,24 @@ const JobList = (props) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
-        return false;
+      return false;
     }
 
     try {
-        const response = await addBid(jobData);
-        //console.log(jobId)
-        if (!response.ok) {
-            throw new Error('something went wrong!');
-        }
+      const response = await addBid(jobData);
+      //console.log(jobId)
+      if (!response.ok) {
+        throw new Error("something went wrong!");
+      }
 
-        const updatedJob = await response.json();
-        setJobData(updatedJob);
+      const updatedJob = await response.json();
+      setJobData(updatedJob);
 
-        alert('Thank you for placing a bid!'); 
-
+      alert("Thank you for placing a bid!");
     } catch (err) {
-        console.error(err);
+      console.error(err);
     }
-};
+  };
 
   if (!jobDataLength) {
     return <h2>LOADING...</h2>;
@@ -61,28 +58,28 @@ const JobList = (props) => {
 
   return (
     <>
-      <Jumbotron fluid className='text-light bg-dark'>
+      <Jumbotron fluid className="text-light bg-dark">
         <Container>
           <h1>Jobs!</h1>
         </Container>
       </Jumbotron>
       <Container>
         <h2>
-          {jobData.length
-            ? `Viewing ${jobData.length} ${jobData.length === 1 ? 'job' : 'jobs'}:`
-            : 'You have no Jobs!'}
+          {jobData.length ? `Viewing ${jobData.length} ${jobData.length === 1 ? "job" : "jobs"}:` : "You have no Jobs!"}
         </h2>
         <CardColumns>
           {jobData.map((jobs) => {
             //console.log(jobs._id)
-            
+
             return (
-              <Card key={jobs._id} border='dark'>
+              <Card key={jobs._id} border="dark">
                 <Card.Body>
-                  <Card.Title><Link to={`/onejob/${jobs._id}`}> Title: {jobs.title}</Link></Card.Title>
-                  <p className='small'>Description: {jobs.description}</p>
-                  <p className='small'>User: {jobs.username}</p>
-                  <input type='integer' id="bid" name="bid" value={jobs.bidAmount} placeholder='Place your bid!'></input>
+                  <Card.Title>
+                    <Link to={`/onejob/${jobs._id}`}> Title: {jobs.title}</Link>
+                  </Card.Title>
+                  <p className="small">Description: {jobs.description}</p>
+                  <p className="small">User: {jobs.username}</p>
+                  <input type="integer" id="bid" name="bid" value={jobs.bidAmount} placeholder="Place your bid!"></input>
                   <Button onClick={() => handleBidJob(jobData._Id)}>Submit</Button>
                 </Card.Body>
               </Card>
